8397a374dd47425ab93c0985e4d53b8b0afd0565
SediqeZangane <67832722+SediqeZangane@users.noreply.github.com>
2020-07-04 22:10:23 +0430

    try for conflict

--- app/src/main/java/com/example/androidpart2/testforgitActivity2.java
+++ app/src/main/java/com/example/androidpart2/testforgitActivity2.java
@@ -10,5 +10,6 @@ public class testforgitActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testforgit2);
+        //try for conflict
     }
 }
