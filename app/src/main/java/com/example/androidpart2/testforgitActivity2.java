06a48c9c76d080e506e266524d5cb48a0307b220
ASUS <se.zanganeh@yahoo.com>
2020-07-04 22:11:18 +0430

    //try for conflict

--- app/src/main/java/com/example/androidpart2/testforgitActivity2.java
+++ app/src/main/java/com/example/androidpart2/testforgitActivity2.java
@@ -10,5 +10,7 @@ public class testforgitActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testforgit2);
+        //try for conflict
+
     }
 }
