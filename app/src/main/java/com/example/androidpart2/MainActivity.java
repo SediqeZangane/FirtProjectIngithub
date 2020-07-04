a760591ed4c0b7a91944d8e8a8a93c7be38b6c06
unknown <you@example.com>
2020-07-04 20:50:59 +0430

    first

---
+++ app/src/main/java/com/example/androidpart2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidpart2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LinearLayout mainlayout=findViewById(R.id.mamad);
+    }
+}
---
+++ app/src/main/java/part2.java
@@ -0,0 +1,14 @@
+import android.os.Bundle;
+
+
+import com.example.androidpart2.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class part2 extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.part2);
+    }
+}
